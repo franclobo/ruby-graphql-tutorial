5d4c37f2cc57a64accb4f169056d73c8767abd4d
Francisco Borja Lobato <fjbl2788@gmail.com>
2023-06-05 18:26:43 -0500

    Queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,16 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # queries are represented as fields
+    # `all_links` is automatically camelcased to `allLinks`
+
+    field :all_links, [LinkType], null: false
+
+    # This method is invoqued when 'all_link' fields is being resolved
+
+    def all_links
+      Link.all
     end
+
   end
 end
